973f32adf2c07f0b512d889bc4d8a3894d7d5fe4
coderdaduat <coderdaudat@gmail.com>
2017-04-14 16:37:05 +0700

    fix server

--- server.py
+++ server.py
@@ -69,4 +69,5 @@ def handle_recording():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
